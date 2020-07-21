b8e20b3ae2fc5e3b31c158b90152076a3e3330c8
christophe gueroult <christophegueroult@gmail.com>
2020-07-21 08:49:28 +0100

    transclusion avec ng-content

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,16 +1,19 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginModule } from '../login/login.module';
-import { TextModule } from '../text/text.module';
-import { IconsModule } from '../icons/icons.module';
-import { UiModule } from '../ui/ui.module';
-import { TemplatesModule } from '../templates/templates.module';
+import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { IconsModule } from '../icons/icons.module';
+import { LoginModule } from '../login/login.module';
+import { TemplatesModule } from '../templates/templates.module';
+import { TextModule } from '../text/text.module';
+import { UiModule } from '../ui/ui.module';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent, NavComponent, FooterComponent],
   imports: [
     CommonModule
   ],
@@ -21,6 +24,9 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     UiModule,
     TemplatesModule,
     NgbModule,
+    HeaderComponent,
+    NavComponent,
+    FooterComponent,
   ]
 })
 export class CoreModule { }
