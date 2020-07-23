98cd1c2054bfa8c1a45cf1ee75df351e22553c61
christophe gueroult <christophegueroult@gmail.com>
2020-07-23 13:45:50 +0100

    :host-context with encapsulation

--- src/app/library/library.module.ts
+++ src/app/library/library.module.ts
@@ -3,15 +3,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ButtonComponent } from './components/button/button.component';
 import { TableLightComponent } from './components/table-light/table-light.component';
+import { TableDarkComponent } from './components/table-dark/table-dark.component';
 
 
 
 @NgModule({
-  declarations: [TableLightComponent, ButtonComponent],
+  declarations: [TableLightComponent, ButtonComponent, TableDarkComponent],
   imports: [
     CommonModule,
     RouterModule
   ],
-  exports: [TableLightComponent, ButtonComponent]
+  exports: [TableLightComponent, ButtonComponent, TableDarkComponent]
 })
 export class LibraryModule { }
